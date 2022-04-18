abe5ff1cb5979f14491a47552dd5ffef3b78035c
Lodovico Giaretta <lodovico@kth.se>
2022-04-18 11:11:09 +0200

    Fixed schemawalk walker

--- src/walkers/schemawalk.py
+++ src/walkers/schemawalk.py
@@ -1,15 +1,17 @@
 import networkx
+import numpy as np
 import random
 from collections import defaultdict
 
 def schemawalk(graph, dataset_params, N, L, alpha, *args):
+    edge_type_map = {edge_type: edge_type_id for edge_type_id, edge_type in enumerate(np.unique(list(networkx.get_edge_attributes(graph, 'type').values())))}
     graph_adj = {}
     for i in graph.nodes:
         neighs = defaultdict(list)
         for j in graph.adj[i]:
-            neighs[j[0]].append(j)
+            neighs[edge_type_map[''.join(sorted([i[0],j[0]]))]].append(j)
         graph_adj[i] = neighs
-    num_edge_types = len(set(networkx.get_edge_attributes(graph, 'type').values()))
+    num_edge_types = len(edge_type_map)
     return higher_order_walk, higher_order_walks(graph, N), (num_edge_types, alpha, graph_adj, L)
     
 def higher_order_walks(graph, num_walks_per_node):
